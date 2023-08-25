a52d8e7518af35e2fb99180ab5bae07437b84f21
Victor Alves <alvesmlktop@gmail.com>
2023-08-25 13:54:12 -0300

    adicionado os primeiros parametros da aplicaçao

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Concluí a aula 01 e agora estou mergulhando em Java!(como se fosse a primeira vez hahahaha aff)");
+        System.out.println("esse é o Screen Match");
+        System.out.println("Top Gun: MAVERICK ");
+        // incluindo ano de lançamento, nota do filme e sinopse
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento : " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+         double media = (9.8 + 6.3 + 8.00) /3;
+
+        String sinopse = """
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80
+                muito bom!
+                Ano de lançamento : 
+                """ + anoDeLancamento;
+        System.out.println(sinopse);
     }
 }
