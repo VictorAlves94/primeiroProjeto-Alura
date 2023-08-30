11b61d94b6e8ecf119a1fb49cd2b840f22aa7737
Victor Alves <alvesmlktop@gmail.com>
2023-08-30 09:08:26 -0300

    criados dois tipos de loop

--- src/Loop.java
+++ src/Loop.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Loop {
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double mediaAvaliacao = 0;
+        double nota = 0;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("De uma nota para o filme");
+            nota = teclado.nextDouble();
+            mediaAvaliacao += nota;
+        }
+        System.out.println("Media de avaliação" + mediaAvaliacao/3);
+
+    }
 }
--- src/OutroLoop.java
+++ src/OutroLoop.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class OutroLoop {
+import java.util.Scanner;
+
+public class OutroLoop {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double mediaAvaliacao = 0;
+        double nota = 0;
+        int totalNotas = 0;
+
+        while (nota != -1){
+            System.out.println("De uma nota para o filme ou -1 para incerrar");
+            nota = teclado.nextDouble();
+            if (nota != -1) {
+                mediaAvaliacao += nota;
+                totalNotas++;
+            }
+        }
+        if (nota != 0) {
+            System.out.println("Media de avaliação : " + mediaAvaliacao / totalNotas);
+        }
+
+    }
 }
